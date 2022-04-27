3a953fbc3e73f231c84c2ea9e01aaa5ad31b3804
ThiHbs <thiagowarz@outlook.com>
2022-04-26 21:21:31 -0300

    firstCommit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'class-methods-use-this': 'off',
+  },
+};
