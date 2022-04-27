3b29384d01f26d10dc44f5823d436994afe61927
ThiHbs <thiagowarz@outlook.com>
2022-04-27 00:23:02 -0300

    Criado o endpoint get da api de alunos

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
+    'import/first': 'off',
     'class-methods-use-this': 'off',
   },
 };
--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,8 +1,16 @@
+import Aluno from '../models/Aluno';
+
 class HomeController {
-  index(req, res) {
-    res.json({
-      tudoCerto: true,
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: 'Rodrigo1',
+      sobrenome: 'Baitola1',
+      email: 'baitola@gmail.com',
+      idade: '26',
+      peso: '200',
+      altura: '1.89',
     });
+    res.json(novoAluno);
   }
 }
 
